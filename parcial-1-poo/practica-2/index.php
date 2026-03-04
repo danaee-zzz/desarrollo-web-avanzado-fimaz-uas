77d6c135924f4cb643061ccf891718370344bafa
danaee-zzz <zzzdannapaola@gmail.com>
2026-03-03 22:22:54 -0700

    Se realizan cambios en el echo de index.php

--- parcial-1-poo/practica-2/index.php
+++ parcial-1-poo/practica-2/index.php
@@ -1,7 +1,5 @@
 <?php
     include 'Admin.php';
     $admin = new Admin('Danna Paola Lizarraga Hernandez','paolahernandez5245@gmail.com');
-    echo '<p>Objeto $admin->getNombre ' . $admin->getNombre() . '</p>';
-    echo '<p>Objeto $admin->getEdad ' . $admin->getCorreo() . '</p>';
-    echo $admin->getRol();
+    echo $admin->getNombre() . "-" .$admin->getCorreo(). "-" .$admin->getRol();
 ?>
