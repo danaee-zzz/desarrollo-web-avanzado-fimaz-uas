4a14c65d87a23028264795111b7a8980cda5713f
danaee-zzz <zzzdannapaola@gmail.com>
2026-04-16 16:54:22 -0700

    Se realizan correcciones

--- parcial-2-pdo/practica-3/practica-transacciones/index.php
+++ parcial-2-pdo/practica-3/practica-transacciones/index.php
@@ -3,7 +3,7 @@
     $host = "localhost";
     $db = "escuela";
     $user = "root";
-    $pass = "851951";
+    $pass = "";
     $charset = "utf8mb4";
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
